cbcc5a79824da2adbf261f4b04821552358b2430
olgavg <olgavg@mail.ru>
2022-11-06 13:20:30 +0300

    Add files via upload

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,19 +1,3 @@
-function mobileMenu() {
-  var navBarNav = document.getElementById("navBarNavMobile");
-  var mobileMenu = document.getElementById("mobileMenu");
-  var searchForm = document.getElementById("searchForm");
-  if (navBarNav.classList.contains('navbr-nav_show')) {
-    navBarNav.classList.remove('navbr-nav_show');
-    mobileMenu.classList.remove('icon-cross');
-    searchForm.style.display = 'none';
-  }
-  else {
-    navBarNav.classList.toggle("navbr-nav_show");
-    mobileMenu.classList.toggle('icon-cross');
-    searchForm.style.display = 'block';
-  }
-}
-
 document.querySelectorAll('.toggle-dropdown').forEach(funcDropDown);
 
 function funcDropDown(dropDown) {
